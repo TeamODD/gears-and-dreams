8f3393a79d727feccf61382877afeae4765d4307
es-e2b <ensu1997@naver.com>
2024-11-22 14:52:00 +0900

    feature: 재료 슬롯 버튼 클릭시 재료 수집되도록 변경

--- Assets/Scripts/MaterialSelection/MaterialSelectionController.cs
+++ Assets/Scripts/MaterialSelection/MaterialSelectionController.cs
@@ -2,10 +2,14 @@ namespace Assets.Scripts.MaterialSelection
 {
     using System;
     using System.Collections;
+    using System.Collections.Generic;
     using UnityEngine;
     
     public class MaterialSelectionController : MonoBehaviour
     {
+        [SerializeField]
+        private CollectedMaterial _collectedMaterial;
+        private Dictionary<Material, int> _targetMaterial;
         [SerializeField]
         private Material[] _materialPool;
         [SerializeField]
@@ -15,12 +19,29 @@ namespace Assets.Scripts.MaterialSelection
         private float _timePerSelection;
         [SerializeField]
         private MaterialSelectionButton[] _selectionSlots;
+        private void Awake()
+        {
+            _targetMaterial=new();
+            for(int i=0;i<5;i++)
+            {
+                int index=UnityEngine.Random.Range(1, _materialPool.Length);
+                _targetMaterial[_materialPool[index]]=_targetMaterial.GetValueOrDefault(_materialPool[index], 0)+1;
+            }
+        }
+        private void Start()
+        {
+            StartCoroutine(ActivateMaterialSelectionMode());
+        }
         public IEnumerator ActivateMaterialSelectionMode()
         {
             _remainingSelectionCount=_totalSelectionCount;
             while(_remainingSelectionCount>0)
             {
-                Array.ForEach(_selectionSlots, selectionSlot=>selectionSlot.Material=_materialPool[UnityEngine.Random.Range(1,_materialPool.Length)]);
+                Array.ForEach(_selectionSlots, selectionSlot=>
+                {
+                    selectionSlot.Material=_materialPool[UnityEngine.Random.Range(1,_materialPool.Length)];
+                    selectionSlot.OnCompleteLightAnimation=_collectedMaterial.CollectMaterial;
+                });
                 float elapsedTime=0f;
                 while(elapsedTime<_timePerSelection)
                 {
