fd50f007d83a978dbf2c89bbf4bd85bc77aaf44a
es-e2b <ensu1997@naver.com>
2024-11-23 23:13:38 +0900

    feature: 마우스 드래그로 톱니바퀴 깎는 클래스 추가

---
+++ Assets/Scripts/Cutting/MeshCreator.cs
@@ -0,0 +1,72 @@
+namespace Assets.Scripts.Cutting
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class MeshCreator : MonoBehaviour
+    {
+        [SerializeField]
+        Material _punchMaterial;
+        [SerializeField]
+        GameObject _parentObject;
+        List<Vector3> verticesList=new();
+        void Update()
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                verticesList=new();
+            }
+            if(Input.GetMouseButton(0))
+            {
+                Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                verticesList.Add(new Vector3(mousePos.x, mousePos.y, 0));
+            }
+            if(Input.GetMouseButtonUp(0))
+            {
+                CreateMesh(verticesList);
+            }
+
+        }
+        private void CreateMesh(List<Vector3> verticesList)
+        {
+            int length=verticesList.Count;
+            for(int i=1;i<length-1;i++)
+            {
+                verticesList.Add(ReflectPoint(verticesList[i], verticesList[0], verticesList[length-1]));
+            }
+            Vector3[] vertices= verticesList.ToArray();
+            Mesh mesh = new Mesh();
+
+            List<int> intsList=new();
+            for(int i=1;i<vertices.Length-1;i++)
+            {
+                intsList.Add(0);
+                intsList.Add(i);
+                intsList.Add(i+1);
+            }
+            int[] triangles = intsList.ToArray();
+
+            mesh.vertices = vertices;
+            mesh.triangles = triangles;
+
+            GameObject meshObject=new();
+            if(_parentObject!=null)
+            {
+                meshObject.transform.SetParent(_parentObject.transform, true);
+            }
+
+            MeshFilter meshFilter = meshObject.AddComponent<MeshFilter>();
+            meshFilter.mesh = mesh;
+
+            MeshRenderer meshRenderer = meshObject.AddComponent<MeshRenderer>();
+            meshRenderer.material = new Material(_punchMaterial);
+        }
+        private Vector3 ReflectPoint(Vector3 point, Vector3 linePoint1, Vector3 center)
+        {
+            Vector3 lineDir = (linePoint1 - center)/2;
+            Vector3 reflected = (lineDir+center)*2 - point;
+
+            return reflected;
+        }
+    }
+}
